f3ab3bc5ff72dc125fe8e357c5988fa9985a5f56
jihoo <jhkim@spa.hanyang.ac.kr>
2021-05-06 09:58:31 +0900

    .

--- get_pointcloud.py
+++ get_pointcloud.py
@@ -1,11 +1,16 @@
-import numpy as np
-from numpy.lib.function_base import angle
-import pyrealsense2 as rs
 import matplotlib.pyplot as plt
-import cv2
-from utils import plot_box
+import numpy as np
+import pyrealsense2 as rs
+import argparse
 
-from pyntcloud import PyntCloud
+from utils import plot_box, count_points_in_box
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--flip', action='store_true')
+parser.add_argument('--angle', type=float)
+args = parser.parse_args()
+
 
 
 pipe = rs.pipeline()
@@ -28,77 +33,78 @@ pc = rs.pointcloud()
 
 while True:
     frames = pipe.wait_for_frames()
-    aligned_frames = align.process(frames)
-
-    aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
-    color_frame = aligned_frames.get_color_frame()
-
-    color_image = np.asanyarray(color_frame.get_data())
-    depth_image = np.asanyarray(aligned_depth_frame.get_data())
-
     depth_frame = frames.get_depth_frame()
     color_frame = frames.get_color_frame()
-    dec_filter = rs.decimation_filter ()   # Decimation - reduces depth frame density
-    spat_filter = rs.spatial_filter()          # Spatial    - edge-preserving spatial smoothing
-    temp_filter = rs.temporal_filter()    # Temporal   - reduces temporal noise
-    frames = pipe.wait_for_frames()
-    depth_frame = frames.get_depth_frame()
+
+    # point cloud post process
+    dec_filter = rs.decimation_filter()
+    spat_filter = rs.spatial_filter()
+    temp_filter = rs.temporal_filter()
     filtered = dec_filter.process(depth_frame)
     filtered = spat_filter.process(filtered)
     filtered = temp_filter.process(filtered)
-
     depth_frame = filtered
-    pc.map_to(color_frame)
-    points = pc.calculate(depth_frame)
 
-    # Visualize point cloud
+    pc.map_to(color_frame)
+
+    points = pc.calculate(depth_frame)
     v, t = points.get_vertices(), points.get_texture_coordinates()
     verts = np.asanyarray(v).view(np.float32).reshape(-1, 3)  # xyz
-    texcoords = np.asanyarray(t).view(np.float32).reshape(-1, 2)  # uv
+    # texcoords = np.asanyarray(t).view(np.float32).reshape(-1, 2)  # uv
 
     # set positions & ranges
     shaft_position = (0.15, -0.2)
-    door_positions = [(0.02, 0.785), (0.05, 1.)]
+    door_positions = [(0.02, 0.785, 0.), (0.05, 1., 0.1)]
     x_range = [-2, 2]
     y_range = [-0.5, 3]
     z_range = [-1, 0.5]
-    angle_thr_degree = 20
-    angle_thr = angle_thr_degree * np.pi / 180
+    angle_degree = 20
+    if args.angle is not None:
+        angle_degree = args.angle
+    angle = angle_degree * np.pi / 180
+    box_width = 0.1
+    box_height = 0.1
 
-    x_positions = verts[:, 0]
-    y_positions = verts[:, 2]
-    z_positions = verts[:, 1]
+    x_coords = verts[:, 0]
+    y_coords = verts[:, 2]
+    z_coords = verts[:, 1]
     
-    # x_positions *= -1
-    # z_positions *= -1
+    if args.flip:
+        x_coords *= -1
+        z_coords *= -1
 
-    idx = np.where(((x_range[0] < x_positions) & (x_positions < x_range[1])) &\
-                   ((y_range[0] < y_positions) & (y_positions < y_range[1])) &\
-                   ((z_range[0] < z_positions) & (z_positions < z_range[1])))[0]
-    idx_z = np.where((0.19 < z_positions) & (z_positions < 0.21))[0]
-    z_line_x = x_positions[idx_z]
-    z_line_y = y_positions[idx_z]
-    x_positions = x_positions[idx]
-    y_positions = y_positions[idx]
+    idx = np.where(((x_range[0] < x_coords) & (x_coords < x_range[1])) &\
+                   ((y_range[0] < y_coords) & (y_coords < y_range[1])) &\
+                   ((z_range[0] < z_coords) & (z_coords < z_range[1])))[0]
+    idx_z = np.where((0.19 < z_coords) & (z_coords < 0.21))[0]
+    x_coords = x_coords[idx]
+    y_coords = y_coords[idx]
+    z_coords = z_coords[idx]
 
-    plt.scatter(x_positions, y_positions, s=0.1, color='silver')
+    plt.scatter(x_coords, y_coords, s=0.1, color='silver')
     plt.scatter(shaft_position[0], shaft_position[1], s=10, color='green', label='shaft')
     plt.scatter(0., 0., s=10, color='black', label='camera')
+    num_points = 0
     for door_position in door_positions:
-        des_position_x = (door_position[0] - shaft_position[0]) * np.cos(angle_thr)\
-                        - (door_position[1] - shaft_position[1]) * np.sin(angle_thr) + shaft_position[0]
-        des_position_y = (door_position[0] - shaft_position[0]) * np.sin(angle_thr)\
-                        + (door_position[1] - shaft_position[1]) * np.cos(angle_thr) + shaft_position[1]
+        des_position_x = (door_position[0] - shaft_position[0]) * np.cos(angle)\
+                        - (door_position[1] - shaft_position[1]) * np.sin(angle) + shaft_position[0]
+        des_position_y = (door_position[0] - shaft_position[0]) * np.sin(angle)\
+                        + (door_position[1] - shaft_position[1]) * np.cos(angle) + shaft_position[1]
+        des_position_z = door_position[2]
 
         plt.scatter(des_position_x, des_position_y, s=10, color='red')
         plt.scatter(door_position[0], door_position[1], s=10, color='blue')
-        plot_box((des_position_x, des_position_y), scale=0.1)
-        # plt.scatter(z_line_x, z_line_y, s=5, color='black')
+        plot_box((des_position_x, des_position_y), box_width, box_height)
+        num_points += count_points_in_box(x_coords, y_coords, z_coords,
+                                  (des_position_x, des_position_y),
+                                  box_width, box_height, des_position_z)
     plt.xlim(x_range)
     plt.ylim(y_range)
     ax = plt.gca()
     ax.set_aspect('equal', adjustable='box')
     plt.legend(loc='lower left')
+    if num_points > 0:
+        plt.xlabel('WARNING!!', fontsize=20)
     plt.pause(0.05)
     plt.cla()
 
--- utils.py
+++ utils.py
@@ -1,12 +1,29 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+import numpy as np
 
 
-def plot_box(point, scale):
+def plot_box(point, width, height):
     ax = plt.gca()
-    box = patches.Rectangle((point[0] - scale/2, point[1] - scale/2),
-                            width=scale,
-                            height=scale,
-                            edgecolor='r',
-                            facecolor='none')
+    box = patches.Rectangle((point[0] - width / 2,
+                             point[1] - height / 2),
+                             width=width,
+                             height=height,
+                             edgecolor='r',
+                             facecolor='none')
     ax.add_patch(box)
+
+def count_points_in_box(x_coords, y_coords, z_coords,
+    point, width, height, z_coord, offset=1):
+    idx = np.where((((point[0] - width / 2) < x_coords) &\
+                    ((point[0] + width / 2) > x_coords) &\
+                    ((point[1] - height / 2) < y_coords) &\
+                    ((point[1] + height / 2) > y_coords) &
+                    ((z_coord - offset) < z_coords) &
+                    ((z_coord + offset) > z_coords)))[0]
+    num_points_in_box = idx.shape[0]
+    
+    # coloring points in box
+    plt.scatter(x_coords[idx], y_coords[idx], s=1, color='lightcoral')
+
+    return num_points_in_box
